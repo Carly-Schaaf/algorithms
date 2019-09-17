c0ec16bdd5564b44e5c79b215d35229792d7b34b
Carly Schaaf <carlyschaaf@berkeley.edu>
2019-09-16 21:32:21 -0700

    Complete linked list implementation

--- linked_list/linked_list_project/lib/linked_list.js
+++ linked_list/linked_list_project/lib/linked_list.js
@@ -39,51 +39,78 @@ class LinkedList {
 
     // TODO: Implement the addToTail method here
     addToTail(val) {
-        if (this.head === null && this.tail === null) return null;
         this.length += 1;
-        const oldTail = this.tail;
         const newTail = new Node(val);
-        oldTail.next = newTail;
+        if (this.tail) {
+            const oldTail = this.tail;
+            oldTail.next = newTail;
+        }
+        if (!this.head) this.head = newTail;
+        this.tail = newTail;
         newTail.next = null;
-        this.tail = oldTail;
+        return this;
     }
     // head --> node --> node --> tail --> null
 
     // TODO: Implement the removeTail method here
     removeTail() {
-        if (this.head === null && this.tail === null) return null;
-        this.length -= 1;
-
-        const oldTail = this.tail;
-        let newTail;
-        while (newTail.next !== oldTail) {
-            newTail = newTail.next;
+        if (this.length === 0) return undefined;
+        if (this.tail === null || this.head === null) return null;
+        if (this.length === 1) {
+            this.tail = null;
+            this.head = null;
         }
-        newTail.next = null;
-        this.tail = newTail;
+        this.length -= 1;
+        const oldTail = this.tail;
+        let newTail = this.head;
+        for (let i = 0; i < this.length; i++) {
+            if (newTail.next === oldTail) {
+                newTail.next = null;
+                this.tail = newTail;
+                return oldTail;
+            } else {
+                newTail = newTail.next;
+            }         
+        }
     }
 
     // TODO: Implement the addToHead method here
     addToHead(val) {
         this.length += 1;
         const newHead = new Node(val);
-        newHead.next = this.head;
+        if (this.head) {
+            newHead.next = this.head
+        }
+        if (this.tail) {
+            newHead.next = this.tail;
+        } else {
+            this.tail = newHead;
+        }
         this.head = newHead;
+        return this;
     }
 
     // TODO: Implement the removeHead method here
     removeHead() {
+        if (this.length === 0) return undefined;
+        const oldHead = Object.assign({}, this.head);
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            const newHead = this.head.next;
+            this.head.next = null;
+            this.head = newHead;
+        }
         this.length -= 1;
-        const newHead = this.head.next;
-        this.head.next = null;
-        this.head = newHead;
+        return oldHead;
     }
 
     // TODO: Implement the contains method here
     contains(target) {
-        const curr = this.head;
+        let curr = this.head;
         while (curr !== null) {
-            if (curr.value = target) return true;
+            if (curr.value === target) return true;
             curr = curr.next;
         }
         return false;
@@ -107,7 +134,7 @@ class LinkedList {
         for (let i = 0; i <= index; i++) {
             if (node === null) return false;
             if (i === index) {
-                node.val = val;
+                node.value = val;
                 return true;
             }
             node = node.next;
@@ -119,16 +146,19 @@ class LinkedList {
         let nextNode = this.head;
         let prevNode;
         for (let i = 0; i <= index; i++) {
-            if (nextNode === null) return null;
+            if (nextNode === null) return false;
             if (i === index) {
+                this.length += 1;
                 const newNode = new Node(val);
                 newNode.next = nextNode;
                 if (prevNode) prevNode.next = newNode;
                 if (i === 0) this.head = newNode;
                 if (i === this.length) this.tail = newNode;
+                return true;
             }
-            prevNode = node;
-            node = node.next;
+            prevNode = nextNode;
+            nextNode = nextNode.next;
+
         }
     }
 
@@ -137,24 +167,32 @@ class LinkedList {
         let currNode = this.head;
         let prevNode;
         for (let i = 0; i <= index; i++) {
-            if (currNode === null) return null;
+            if (!currNode) return undefined;
             if (i === index) {
-                const newNode = new Node(val);
-                newNode.next = currNode;
-                if (prevNode) prevNode.next = newNode;
-                if (i === 0) this.head = newNode;
-                if (i === this.length) this.tail = newNode;
+                this.length -= 1;
+                if (prevNode) prevNode.next = currNode.next;
+                if (i === 0) this.head = currNode.next;
+                if (i === this.length) {
+                 prevNode.next = null;
+                 this.tail = prevNode;   
+                }
+                return currNode;
             }
-            prevNode = node;
-            node = node.next;
+            prevNode = currNode;
+            currNode = currNode.next;
         }
     }
 
     // TODO: Implement the size method here
     size() {
-
+        return this.length;
     }
 }
 
 exports.Node = Node;
 exports.LinkedList = LinkedList;
+
+const linkedList = new LinkedList();
+linkedList.addToTail('C');
+linkedList.addToHead('A');
+linkedList.insert(1, 'B');
