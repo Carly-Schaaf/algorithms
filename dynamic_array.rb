7276c120f2b3af0c32c4674208639c1777d2bcfd
Carly Schaaf <carlyschaaf@berkeley.edu>
2019-09-20 15:29:30 -0700

    fix bug in unshift

--- dynamic_array.rb
+++ dynamic_array.rb
@@ -3,6 +3,7 @@ class DArray
     def initialize
         @static = Array.new(5)
         @count = 0
+        @end_idx = 0
         @start_idx = 0
     end
 
@@ -11,12 +12,32 @@ class DArray
     end
 
     def push(el)
-        unless full?
-            @static[@count] = el 
-            @count += 1
-            return true
+        back_resize! if full?
+        @static[@end_idx] = el 
+        @end_idx += 1
+        @count += 1
+        return true
+    end
+
+    def pop
+        unless empty?
+            @end_idx -= 1
+            el = @static[@end_idx]
+            @static[@end_idx] = nil
+            @count -= 1
+            return el
         end
         false
+    end
+
+    def back_resize!
+        @static += Array.new(@static.length)
+    end
+
+    def front_resize!
+        @start_idx += @static.length
+        @end_idx += @static.length
+        @static = Array.new(@static.length) + @static
     end
 
     def full?
@@ -27,55 +48,48 @@ class DArray
         @openings == @static.length
     end
 
-    def pop
-        unless empty?
-            el = @static[@count]
-            @static[@count] = nil
-            @count -= 1
-            return el
-        end
-        false
-    end
-
     def front 
+        return "empty" if empty?
         @static[@start_idx]
     end
 
     def rear 
-        @static[@count - 1]
+        return "empty" if empty?
+        @static[@end_idx - 1]
+    end
+
+    def unshift(el)
+        debugger
+        front_resize! if full?  
+        @static[@start_idx] = el
+        @end_idx += 1
+        @count += 1
+        return true
     end
 
     def shift
         unless empty?
-            el = @static[@start_idx]
             @start_idx += 1
+            @end_idx -= 1
+            el = @static[@start_idx]
+            @static[@start_idx] = nil
             @count -= 1
             return el
         end
         false
     end
 
-    def unshift(el)
-        if @start_idx > 0
-            @count += 1
-            @start_idx -= 1
-            @static[@start_idx] = el
-            return true
-        end
-        false
-    end
 end
 
 d = DArray.new 
-p d.push(1) # true
-p d.push(2) # true
-p d.push(3) # true
-p d.push(4) # true
-p d.push(5) # true
-p d.push(6) # false
-p d.rear # 5
-p d.front # 1
-p d.shift # 1
+p d.unshift(1) # true
+p d.unshift(2) # true
+p d.unshift(3) # true
+p d.unshift(4) # true
+p d.unshift(5) # true
+p d.unshift(6) # true
+p d.rear # 1
+p d.front # 6
 p d.unshift(0) # true
 p d.front # 0
-p d.rear # 5
+p d.rear # 6
