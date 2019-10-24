e1b7fa9bffe733bcfd00900593b2e8e4ebdc9ec4
Carly Schaaf <carlyschaaf@berkeley.edu>
2019-10-24 13:16:39 -0700

    Finish tree order JS implementation

--- tree_order_project/lib/leet_code_105.js
+++ tree_order_project/lib/leet_code_105.js
@@ -5,5 +5,15 @@ const { TreeNode } = require('./tree_node.js');
 
 
 function buildTree(preorder, inorder) {
+    if (inorder.length === 0) { return null; }
 
+    const root = new TreeNode(preorder.shift());
+    const rootIdx = inorder.indexOf(root.val);
+    const leftTree = inorder.slice(0, rootIdx);
+    const rightTree = inorder.slice(rootIdx + 1, inorder.length);
+
+    root.left = buildTree(preorder, leftTree);
+    root.right = buildTree(preorder, rightTree);
+
+    return root;
 }
