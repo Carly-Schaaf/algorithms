0c2208f6f7d52897b934e26a1abed831a9a69b2c
Carly Schaaf <carlyschaaf@berkeley.edu>
2019-12-07 21:47:01 -0800

    finish longest palidrome

--- longest_p.rb
+++ longest_p.rb
@@ -1,45 +1,53 @@
 # @param {String} s
 # @return {String}
 def longest_palindrome(s)
-    return "" if s.length == 0
-    max_length = 1
     max = s[0]
-    
-    s.each_char.with_index do |_, i|
-        next if i == s.length - 1
 
-        two_letter_sub = s[i] + s[i + 1]
-        if is_palidrome?(two_letter_sub)
-            max_length = 2
-            max = two_letter_sub
-        end
+    s.each_char.with_index do |el, i|
 
-        offset = 1
-        start = i - offset
-        last = i + offset 
+        # for odd substrings - when middle is el
+        start_idx = i - 1
+        last_idx = i + 1
+        result_odd = s.grow_substring(max, start_idx, last_idx)
 
-        next if start < 0
+        # for even substrings - when middle is between el and el.next
+        start_idx = i 
+        last_idx = i + 1
+        result_even = s.grow_substring(max, start_idx, last_idx)
 
-        sub = s[start..i + offset]
-        while is_palidrome?(sub)
-            max_length = sub.length 
-            max = sub 
-
-            offset += 1
-            start = i - offset
-            last = i + offset 
-
-            break if start < 0 || last > s.length - 1
-
-            sub = s[start..i + last]
-        end
+        max = [max, result_even, result_odd].max { |a, b| a.length <=> b.length }
     end
-    
-    max
+
+    # if max is null due to indexing into it on line 4, return ""
+    return max || ""
 end
 
-def is_palidrome?(sub)
+class String 
+    def grow_substring(max, start_idx, last_idx)
+        offset = 1
+
+        until (start_idx < 0) || (last_idx >= self.length)
+            sub = self[start_idx..last_idx]
+            break unless ends_match?(sub)
+
+            if sub.length > max.length
+                max = sub
+            end
+
+            offset += 1
+            start_idx -= 1
+            last_idx += 1
+        end
+
+        return max
+    end
+end
+
+def ends_match?(sub)
     sub[0] == sub[-1]
 end
 
-puts longest_palindrome("aaaa")
+puts longest_palindrome("aabaabc") == "aabaa"
+puts longest_palindrome("aaaaaa") == "aaaaaa"
+puts longest_palindrome("xx") == "xx"
+puts longest_palindrome("y") == "y"
