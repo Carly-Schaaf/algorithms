b7f3c8dca4760992a55ec691b0c5a165969ab039
Carly Schaaf <carlyschaaf@berkeley.edu>
2020-01-15 15:28:31 -0800

    Finish problem

--- climb_stairs.py
+++ climb_stairs.py
@@ -5,16 +5,18 @@ def climbStairs(n):
     :rtype: int
     """
     table = [None] * (n + 1)
-    table[1] = 1
-    table[2] = 2
 
-    for step in range(3, n + 1):
-        step1 = step - 1
-        step2 = step - 2
-
-        table[step] = table[step1] + table[step2]
+    for step in range(1, n + 1):
+        if step == 1:
+            table[1] = 1
+        elif step == 2:
+            table[2] = 2
+        else:
+            step1 = step - 1
+            step2 = step - 2
+            table[step] = table[step1] + table[step2]
 
     return table[-1]
 
-print(climbStairs(6))
+print(climbStairs(1))
 
